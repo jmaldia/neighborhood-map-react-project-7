c52a0df8c8e9dffeaf424f9ac0750648ce78aefb
Jon Maldia <jon@maldia.net>
2018-10-07 21:55:49 -0400

    Install google-map-react, add sample component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import GoogleMapReact from 'google-map-react'
+
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
 
 class App extends Component {
+  static defaultProps = {
+    center: {
+      lat: 40.81,
+      lng: -74.13
+    },
+    zoom: 11
+  };
+
   render() {
     return (
       <div className="App">
@@ -19,6 +30,18 @@ class App extends Component {
           >
             Learn React
           </a>
+
+          <GoogleMapReact
+          bootstrapURLKeys={{ key: /* YOUR KEY HERE */ }}
+          defaultCenter={this.props.center}
+          defaultZoom={this.props.zoom}
+        >
+          <AnyReactComponent
+            lat={40.8167447}
+            lng={-74.1380044}
+            text={'Kreyser Avrora'}
+          />
+        </GoogleMapReact>
         </header>
       </div>
     );
