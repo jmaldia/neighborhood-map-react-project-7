e0008afae903d9e93199b90bf5d0a30d385317c9
Jon Maldia <jon@maldia.net>
2018-10-11 09:22:55 -0400

    Add placeholder image - FS limit reached

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -20,11 +20,9 @@ class SideBar extends Component {
                         this.props.locations.map(location => {
                             return (
                                 <li className="Places" key={location.id}>
+                                    <img src="https://via.placeholder.com/150x150" width="100" alt="my restaurant"></img>
                                     <h3>{location.name}</h3>
-                                    {
-                                        location.location.formattedAddress.map(addr => <p>{addr}</p>)
-                                    }
-                                    <img src={this.props.photo} width="100" alt="my restaurant"></img>
+                                    { location.location.formattedAddress.map(addr => <p>{addr}</p>) }
                                 </li>
                             )
                         })
@@ -36,3 +34,5 @@ class SideBar extends Component {
 }
 
 export default SideBar
+
+// add to LI later > <img src={this.props.photo} width="100" alt="my restaurant"></img>
