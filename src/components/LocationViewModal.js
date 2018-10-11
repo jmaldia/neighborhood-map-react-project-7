7a496699a96b7a76ca8358aa257ded7e29e360c8
Jon Maldia <jon@maldia.net>
2018-10-11 09:54:20 -0400

    Add new modal component

---
+++ src/components/LocationViewModal.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import '../App.css'
+
+class LocationViewModal extends Component {
+    componentDidMount() {
+    }
+
+    render() {
+        return(
+            <div className="LocationViewModal">
+                This is the modal
+            </div>
+        )
+    }
+}
+
+export default LocationViewModal
--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -2,10 +2,6 @@ import React, { Component } from 'react'
 import '../App.css'
 
 class SideBar extends Component {
-    state = {
-        locations: this.props.locations
-    }
-
     componentDidMount() {
     }
 
