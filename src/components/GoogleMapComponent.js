268ee8f8ca3cd27bfb30c7fbc1e150f8cdb6a00f
Jon Maldia <jon@maldia.net>
2018-10-15 23:14:29 -0400

    Refactor to use react-google-map instead of google-map-react

---
+++ src/components/GoogleMapComponent.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react'
+import { withScriptjs, withGoogleMap, GoogleMap, Marker, InfoWindow } from "react-google-maps"
+import { keys } from '../keys';
+import '../App.css'
+
+const MapComponent = withScriptjs(withGoogleMap(props =>
+  <GoogleMap
+    defaultZoom={15}
+    zoom={15}
+    defaultCenter={{ lat: 40.8257712, lng: -74.1074718 }}
+    center={{ lat: 40.8257712, lng: -74.1074718 }}
+  >
+    { 
+        props.markers && props.markers.filter(marker => marker.isVisible).map((marker) => (
+            <Marker 
+                key={`m-${marker.id}`} 
+                position={{ lat: marker.lat, lng: marker.lng }} 
+                onClick={() => props.clickMarker(marker) }
+            >
+                { marker.isOpen && (
+                    <InfoWindow>
+                        <h3>{marker.name}<br />{marker.address}</h3>
+                    </InfoWindow>
+                )}
+            </Marker>
+        ))
+    }
+  </GoogleMap>
+))
+
+class GoogleMapComponent extends Component {
+    render() {
+        return (
+            <MapComponent
+                {...this.props}
+                googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&key=${keys.googleMaps.APIkey}`}
+                loadingElement={<div style={{ height: `100%` }} />}
+                containerElement={<div style={{ height: `100vh` }} />}
+                mapElement={<div style={{ height: `100%` }} />}
+            />
+        )
+    }
+}
+
+export default GoogleMapComponent
